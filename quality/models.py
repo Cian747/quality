395aa0da764e723f7d5fe1960454c69920a9a972
Cian <cian.omondi@student.moringaschool.com>
2021-09-02 10:09:19 +0300

    Removed owner class

--- quality/models.py
+++ quality/models.py
@@ -1,23 +1,25 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Owner(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    
 class Category(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
 class Location(models.Model):
     area = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.area
+
 class Images(models.Model):
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length=20)
     image_description = models.TextField()
-    owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     location = models.ManyToManyField(Location)
     posted = models.DateTimeField(auto_now_add=True)
     category = models.ManyToManyField(Category)
