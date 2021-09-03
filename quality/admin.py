0bbc7e496486ccb674e50d0bf57120a3e9f22de6
Cian <cian.omondi@student.moringaschool.com>
2021-09-03 22:48:23 +0300

    created admin page

--- quality/admin.py
+++ quality/admin.py
@@ -3,9 +3,6 @@ from .models import Images,Category,Location
 
 # Register your models here.
 
-class ImageAdmin(admin.ModelAdmin):
-    filter_horizontal = ('category','location')
-
-admin.site.register(Images,ImageAdmin)
+admin.site.register(Images)
 admin.site.register(Category)
 admin.site.register(Location)
